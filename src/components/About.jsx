1249dbdc0b381e0050f89c57fcdc5b751d7d81ad
fuston05 <fuston05@yahoo.com>
2020-05-04 10:22:42 -0400

    adds skills nav link, adds about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,12 @@ const About = () => {
     <div className= 'aboutCont'>
       {/* eslint-disable-next-line */}
       <a id= 'about'></a>
-      <h4 className= 'sectionTitle'>About Me</h4>
+      <div className= 'aboutText'>
+        <h4 className= 'sectionTitle'>About Me</h4>
+        <p>
+          I am a full stack web developer. I am skilled in common front-rnd technologies such as CSS, HTML, JavaScript, React and many more. As for back-end tech, I have experience with REST Api, as well as GraphQL, from expressJS to Apollo Server and PostgresQL. See my <a href= '#skills' target= '_blank' rel= 'noreferrer noopener'>Skills</a> section full details. I am always striving to learn new technologies in an effort to stay on the cutting edge of web development. I have a passion for code and the unlimited possibilities that it offers me as a "creator".
+        </p>
+      </div>
     </div>
   )
 }//end About
--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -13,6 +13,8 @@ const NavMain = () => {
         <ul>
           <li> <a href="#portfolio">Portfolio</a> </li>
           <li> <a href="#about">About</a> </li>
+          <li> <a href="#skills">Skills</a> </li>
+          <li> <a href="#resume">Resume</a> </li>
           <li> <a href="#contact">Contact</a> </li>
         </ul>
       </nav>
