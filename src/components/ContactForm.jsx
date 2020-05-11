61a6c84bf8114fc42588d15df9c436d1a27551d5
fuston05 <fuston05@yahoo.com>
2020-05-11 18:18:42 -0400

    corrects for submission code

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -21,7 +21,7 @@ const ContactForm = () => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({ "form-name": "contact", ...formValue })
     })
       .then(() => alert("Success!"))
       .catch(error => alert(error));
