dbef49e2d597be8f71ef7056ee22f8ed5f8e4711
fuston05 <fuston05@yahoo.com>
2020-09-17 13:42:27 -0400

    refactoring Skills section, still in progress

--- src/assets/data/skillsData.js
+++ src/assets/data/skillsData.js
@@ -5,6 +5,35 @@ export const stylingSkills = [
     "React Styled-Components"
 ]
 
+export const proficientSkills= [
+  "ReactJS",
+  "JavaScript(ES6)",
+  "Python",
+  "HTML",
+  "CSS/LESS/SCSS",
+  "React Styled-Components",
+  "Git",
+  "GitHub",
+  "Zoom",
+  "Slack",
+  "Trello",
+  "Cross-Functional Team Collaboration",
+  "Agile Development"
+];
+
+export const midSkills= [
+  "Redux",
+  "NodeJs",
+  "Express",
+  "RESTful API",
+  "Ant Design",
+];
+
+export const lowSkills= [
+  "GraphQL",
+  "PostgreSQL"
+];
+
 export const frontEndSkills = [
     "HTML",
     "ReactJS",
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,117 +8,57 @@ import alternateColor from '../utils/alternateColor';
 
 //data
 import {
-    frontEndSkills,
-    stylingSkills,
-    backEndSkills,
-    otherSkills
+  frontEndSkills,
+  stylingSkills,
+  backEndSkills,
+  otherSkills
 } from '../assets/data/skillsData';
 
 //styles
 import './Skills.scss';
 
 const Skills = () => {
-    //skills from skillsData.js file
-    const [stSkills, setStSkills] = useState();
-    const [feSkills, setFeSkills] = useState();
-    const [beSkills, setBeSkills] = useState();
-    const [othSkills, setOthSkills] = useState();
+  //skills from skillsData.js file
+  const [stSkills, setStSkills] = useState();
+  const [feSkills, setFeSkills] = useState();
+  const [beSkills, setBeSkills] = useState();
+  const [othSkills, setOthSkills] = useState();
 
-    useEffect(() => {
-        //set skills state on load
-        setStSkills(stylingSkills);
-        setFeSkills(frontEndSkills);
-        setBeSkills(backEndSkills);
-        setOthSkills(otherSkills);
-    }, [stSkills, feSkills, beSkills, othSkills]);
+  useEffect(() => {
+    //set skills state on load
+    setStSkills(stylingSkills);
+    setFeSkills(frontEndSkills);
+    setBeSkills(backEndSkills);
+    setOthSkills(otherSkills);
+  }, [stSkills, feSkills, beSkills, othSkills]);
 
-    return (
-        <div className='skillsCont'>
-            {/* eslint-disable-next-line */}
-            <a id='skills'></a>
+  return (
+    <div className='skillsCont'>
+      {/* eslint-disable-next-line */}
+      <a id='skills'></a>
 
-            <h4 className='sectionTitle'>Skills</h4>
+      <h4 className='sectionTitle'>Skills</h4>
 
-            <div className='skills'>
-                <div className='skill styling'>
-                    {/* FontAwesome Icon */}
-                    <span className=' skillsIcon'><i className="fab fa-css3"></i></span>
-                    <h5>Styling Technologies</h5>
-                    <ul>
-                        {
-                            stSkills && stSkills.map((skill) => {
-                                //alternates light and dark list BG
-                                let colorName = alternateColor();
-                                return (
-                                    <li
-                                        className={colorName}
-                                        key={keyGen()}>{skill}</li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
+      <div className='topSkills'>
+        <h3>Proficient Skills</h3>
+        <span>
+          'React | JavaScript | HTML/CSS | Python '
+        </span>
+      </div>
 
-                <div className=' skill frontEnd'>
-                    {/* FontAwesome Icon */}
-                    <span className='skillsIcon'><i className="fab fa-react"></i></span>
-                    <h5>Front-End Technologies</h5>
-                    <ul>
-                        {
-                            feSkills && feSkills.map(skill => {
-                                //alternates light and dark list BG
-                                let colorName = alternateColor();
-                                return (
-                                    <li
-                                        className={colorName}
-                                        key={keyGen()}>{skill}</li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
+      <div className='medSkills'>
+        <h3>Intermediate Skills(<span>Used a several times</span>)</h3>
+        'Redux | NodeJs | Express | RESTful API | Ant Design'
+      </div>
 
-                <div className=' skill backeEnd'>
-                    {/* FontAwesome Icon */}
-                    <span className='skillsIcon'><i className="fab fa-node-js"></i></span>
-                    <h5>Back-End Technologies</h5>
-                    <ul>
-                        {
-                            beSkills && beSkills.map(skill => {
-                                //alternates light and dark list BG
-                                let colorName = alternateColor();
-                                return (
-                                    <li
-                                        className={colorName}
-                                        key={keyGen()}>{skill}</li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
+      <div className='lowSkills'>
+        <h3>Beginner Skills(<span>Used once or twice</span>)</h3>
+        'GraphQL | Apollo-Server | URQL | GSAP'
+      </div>
 
-                <div className=' skill otherSkills'>
-                    {/* FontAwesome Icon */}
-                    <span className='skillsIcon'><i className="fas fa-code-branch"></i></span>
-                    <h5>Other Skills</h5>
-                    <ul>
-                        {
-                            othSkills && othSkills.map(skill => {
-                                //alternates light and dark list BG
-                                let colorName = alternateColor();
-                                return (
-                                    <li
-                                        className={colorName}
-                                        key={keyGen()}>{skill}</li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
 
-            </div>
-        </div> //end skillsCont
-    )
+    </div> //end skillsCont
+  )
 }
 
 export default Skills;
