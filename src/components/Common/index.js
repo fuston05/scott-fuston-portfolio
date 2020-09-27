ff6765294a760772e8a0aede8c3ae0abe1ec6392
fuston05 <fuston05@yahoo.com>
2020-09-27 11:50:23 -0400

    testing

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,5 +1,4 @@
 // Common components index
-import Canvas from './Particles';
 import SnackBar from './SnackBar/SnackBar';
 import ToTopArrow from './ToTopArrow/ToTopArrow';
 import Header from './Header';
@@ -7,7 +6,6 @@ import Footer from './Footer';
 
 
 export {
-  Canvas,
   SnackBar,
   ToTopArrow,
   Header,
