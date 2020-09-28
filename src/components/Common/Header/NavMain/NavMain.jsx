41b26cecabbc290133a758f956422da8ca1758d0
fuston05 <fuston05@yahoo.com>
2020-09-28 10:40:36 -0400

    tweaked header, nav, and footer postioning

--- src/components/Common/Header/NavMain/NavMain.jsx
+++ src/components/Common/Header/NavMain/NavMain.jsx
@@ -9,7 +9,7 @@ const NavMain = () => {
     <div className='navMainCont'>
       <div className='name'><h1>Scott Fuston</h1></div>
       <hr />
-      <div className='slogan'><h3>~ Endless Possibilities ~</h3></div>
+      <div className='slogan'><h3>Endless Possibilities </h3></div>
       <nav>
         <ul>
           <li>
