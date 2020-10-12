493313bf3ecf50551533e39b65d5667c8e0f224c
scott fuston <fuston05@yahoo.com>
2020-10-12 12:40:16 -0400

    added 2 more projects

--- src/components/Portfolio/ProjectCard.jsx
+++ src/components/Portfolio/ProjectCard.jsx
@@ -1,92 +1,107 @@
-import React from 'react';
+import React from "react";
 
 //styles
-import './ProjectCard.scss';
+import "./ProjectCard.scss";
 
-import NASA from '../../assets/images/NASA.png';
-import comingSoon from '../../assets/images/comingSoon.jpg';
+import NASA from "../../assets/images/NASA.png";
+import comingSoon from "../../assets/images/comingSoon.jpg";
+import storySquad from "../../assets/images/storySquad.png";
+import gameOfLife from "../../assets/images/gameOfLife.png";
 
 const ProjectCard = () => {
   return (
     <>
-      <div className='projectCard'>
+      {/* ********************************** */}
+
+      <div className="projectCard">
+        <h5>Story Squad</h5>
+        <a
+          title="View Large Image"
+          href={storySquad}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={storySquad} alt="storySquad" />
+        </a>
+        <div className="linksCont">
+          <a
+            href="https://github.com/Lambda-School-Labs/Labs26-StorySquad-FE-TeamC"
+            target= '_blank'
+            rel="noreferrer noopener"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://c.storysquad.dev/"
+            target= '_blank'
+            rel="noreferrer noopener"
+          >
+            Live Site
+          </a>
+        </div>
+      </div>
+
+      {/* ********************************** */}
+
+      <div className="projectCard">
         <h5>NASA APOD API</h5>
         <a
-          title='View Large Image'
+          title="View Large Image"
           href={NASA}
-          target='_blank'
-          rel='noreferrer noopener'
-        ><img src={NASA} alt="NASA" /></a>
-        <div className='linksCont'>
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={NASA} alt="NASA" />
+        </a>
+        <div className="linksCont">
           <a
             href="https://github.com/fuston05/nasa-photo-of-the-day"
-            target='_blank'
-            rel='noreferrer noopener'
-          >View My Code
-            </a>
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            View My Code
+          </a>
           <a
             href="https://nasa-apod-fun.netlify.app/"
-            target='_blank'
-            rel='noreferrer noopener'
-          >Live Site
-            </a>
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Live Site
+          </a>
         </div>
       </div>
 
       {/* ********************************** */}
 
-      <div className='projectCard'>
-        <h5>More to Come!</h5>
+      <div className="projectCard">
+        <h5>Conway's Game of Life</h5>
         <a
-          title='View Large Image'
-          href={comingSoon}
-          target='_blank'
-          rel='noreferrer noopener'
-        ><img src={comingSoon} alt="coming soon" /></a>
-        <div className='linksCont'>
+          title="View Large Image"
+          href={gameOfLife}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={gameOfLife} alt="gameOfLife" />
+        </a>
+        <div className="linksCont">
           <a
-            href="#portfolio"
-            // target= '_blank'
-            rel='noreferrer noopener'
-          >GitHub
-      </a>
+            href="https://github.com/fuston05/conways-game-of-life"
+            target= '_blank'
+            rel="noreferrer noopener"
+          >
+            GitHub
+          </a>
           <a
-            href="#portfolio"
-            // target= '_blank'
-            rel='noreferrer noopener'
-          >Live Site
-      </a>
+            href="https://conwaygameoflife.netlify.app/"
+            target= '_blank'
+            rel="noreferrer noopener"
+          >
+            Live Site
+          </a>
         </div>
-      </div>
-
-      {/* ********************************** */}
-
-      <div className='projectCard'>
-        <h5>More to Come!</h5>
-        <a
-          title='View Large Image'
-          href={comingSoon}
-          target='_blank'
-          rel='noreferrer noopener'
-        ><img src={comingSoon} alt="coming soon" /></a>
-        <div className='linksCont'>
-          <a
-            href="#portfolio"
-            // target= '_blank'
-            rel='noreferrer noopener'
-          >GitHub
-      </a>
-          <a
-            href="#portfolio"
-            // target= '_blank'
-            rel='noreferrer noopener'
-          >Live Site
-      </a>
-        </div>
-      
       </div>
     </>
-  )
-}//end ProjectCard
+  );
+}; //end ProjectCard
 
 export default ProjectCard;
