5ad0534d705b0726e512abf643bf1a92e8b1072c
scott fuston <fuston05@yahoo.com>
2020-10-23 20:19:16 -0400

    updates 'about' section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React from "react";
 
 //styles
-import './About.scss';
+import "./About.scss";
 
 const About = () => {
   return (
-    <section className= 'aboutCont'>
+    <section className="aboutCont">
       {/* eslint-disable-next-line */}
-      <a id= 'about'></a>
-      <div className= 'aboutText'>
-        <h4 className= 'sectionTitle'>About Me</h4>
-        <p> &nbsp;&nbsp;&nbsp;
-          I am a full stack web developer, currently studying at Lambda School. I am skilled in common front-end technologies such as CSS, HTML, JavaScript, React and many more. As for back-end tech, I have experience with REST Api, as well as GraphQL, from expressJS to Apollo Server and PostgresQL. See my <a href= '#skills'>Skills</a> section full details. I am always learning new technologies in an effort to stay on the cutting edge of web development. I have a passion for code and the unlimited possibilities that it offers me as a "creator".
+      <a id="about"></a>
+      <div className="aboutText">
+        <h4 className="sectionTitle">About Me</h4>
+        <p>
+          {" "}
+          &nbsp;&nbsp;&nbsp; I am a Full-Stack Web Developer and recent Lambda
+          School Graduate. I am skilled in common front-end technologies such as
+          CSS, HTML, JavaScript, React, Recoil, and much more. As for back-end
+          tech, I have experience with REST API, Express, SQLite, and
+          PostgreSQL. See my portfolio for full details. I am always learning
+          new technologies in an effort to stay on the cutting edge of web
+          development. I have a passion for code and the unlimited possibilities
+          that it offers me as a "creator".
         </p>
       </div>
     </section>
-  )
-}//end About
+  );
+}; //end About
 
 export default About;
