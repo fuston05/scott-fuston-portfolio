169cd08a70de9ab6a202338aa2ed6027f0ba56c4
scott fuston <fuston05@yahoo.com>
2020-10-26 11:56:04 -0400

    adds a description panel to each project card.

--- src/components/Portfolio/ProjectCard.jsx
+++ src/components/Portfolio/ProjectCard.jsx
@@ -10,21 +10,13 @@ import gameOfLife from "../../assets/images/gameOfLife.png";
 // import { Player } from 'video-react';
 
 const ProjectCard = () => {
-
   return (
     <>
       {/* ********************************** */}
 
       <div className="projectCard">
         <h5>Story Squad</h5>
-        <a
-          title="View Large Image"
-          href={storySquad}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img src={storySquad} alt="storySquad" />
-        </a>
+        <img src={storySquad} alt="storySquad" />
         <div className="linksCont">
           <a
             href="https://github.com/Lambda-School-Labs/Labs26-StorySquad-FE-TeamC"
@@ -41,20 +33,21 @@ const ProjectCard = () => {
             Live Site
           </a>
         </div>
+        <div className="description">
+          <h5>Description</h5>
+          <p>
+            This was a Lambda 'Labs' project with a real external stakeholder
+            designed to give students a 2 month long, real-world job experience
+            all while helping the stakeholder bring their idea to life.
+          </p>
+        </div>
       </div>
 
       {/* ********************************** */}
 
       <div className="projectCard">
         <h5>NASA APOD API</h5>
-        <a
-          title="View Large Image"
-          href={NASA}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img src={NASA} alt="NASA" />
-        </a>
+        <img src={NASA} alt="NASA" />
         <div className="linksCont">
           <a
             href="https://github.com/fuston05/nasa-photo-of-the-day"
@@ -71,20 +64,22 @@ const ProjectCard = () => {
             Live Site
           </a>
         </div>
+        <div className="description">
+          <h5>Description</h5>
+          <p>
+            This was a Lambda School project that I really liked and decided to
+            build it further on my own. Get a daily celestial image straight
+            from NASA's APOD API.
+          </p>
+        </div>
       </div>
 
       {/* ********************************** */}
 
       <div className="projectCard">
         <h5>Conway's Game of Life</h5>
-        <a
-          title="View Large Image"
-          href={gameOfLife}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img src={gameOfLife} alt="gameOfLife" />
-        </a>
+
+        <img src={gameOfLife} alt="gameOfLife" />
         <div className="linksCont">
           <a
             href="https://github.com/fuston05/conways-game-of-life"
@@ -100,6 +95,17 @@ const ProjectCard = () => {
           >
             Live Site
           </a>
+        </div>
+        <div className="description">
+          <h5>Description</h5>
+          <p>
+            <i>
+              Disclaimer: The live site is still in progress. It's not yet
+              responsive.
+            </i>
+            This was a Lambda School Computer Science project that I thought was
+            so intriguing that I decided to use it in my portfolio.
+          </p>
         </div>
       </div>
     </>
