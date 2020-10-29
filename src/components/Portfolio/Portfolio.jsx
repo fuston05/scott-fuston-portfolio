669153a41113a24f9849af181764ffe5a90cd634
scott fuston <fuston05@yahoo.com>
2020-10-29 12:01:23 -0400

    adds ability to close video player by clicking off of it in the window

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,16 +4,22 @@ import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
 
 //data
-import projects from '../../assets/data/projects';
+import projects from "../../assets/data/projects";
 
 const Portfolio = () => {
-
   // set state back to false for default
   const [visible, setVisible] = useState(false);
 
   const toggleModal = async () => {
     setVisible(!visible);
   };
+
+  // closes video player modal when you click off of the player
+  window.addEventListener("click", (e) => {
+    if (visible && e.target.classList.contains('modalCont')) {
+      toggleModal();
+    }
+  });
 
   return (
     <section className="portfolioCont">
@@ -22,15 +28,16 @@ const Portfolio = () => {
       <h4 className="sectionTitle">Portfolio</h4>
 
       <div className="cardCont">
-      {
-        projects.map((project) => {
-          return <ProjectCard key= {Date.now() * Math.random()}
-            toggleModal={toggleModal}
-            visible={visible}
-            project= {project}
-          />
-        })
-      }
+        {projects.map((project) => {
+          return (
+            <ProjectCard
+              key={Date.now() * Math.random()}
+              toggleModal={toggleModal}
+              visible={visible}
+              project={project}
+            />
+          );
+        })}
       </div>
     </section>
   );
