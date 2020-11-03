4b8d92327dbe027990f464106a14ec0b1889c5b8
scott fuston <fuston05@yahoo.com>
2020-11-03 18:20:43 -0500

    removes snackbar import no longer used

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,12 +1,10 @@
 // Common components index
-import SnackBar from './SnackBar/SnackBar';
 import ToTopArrow from './ToTopArrow/ToTopArrow';
 import Header from './Header';
 import Footer from './Footer';
 
 
 export {
-  SnackBar,
   ToTopArrow,
   Header,
   Footer,
