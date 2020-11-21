0ba477633988ad9dd73680b732488954099f20b3
scott fuston <fuston05@yahoo.com>
2020-11-21 09:32:40 -0500

    adds react ts particles to header

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,19 +1,104 @@
-import React from 'react';
+import React from "react";
+import Particles from "react-tsparticles";
 
 //components
-import NavMain from './NavMain';
+import NavMain from "./NavMain";
 
 const Header: React.FC = () => {
   return (
-    <header className='headerCont'>
-      <div className='name'><h1>Scott Fuston</h1></div>
+    <header className="headerCont">
+      <div className="name">
+        <h1>Scott Fuston</h1>
+      </div>
       <hr />
-      <div className='slogan'>
+      <div className="slogan">
         <h3>Endless Possibilities </h3>
       </div>
       <NavMain />
+
+      <Particles
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 50,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#899CD8",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 4,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 100,
+            },
+            opacity: {
+              value: 0.7,
+            },
+            shape: {
+              type: "polygon",
+            },
+            size: {
+              random: true,
+              value: 3,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
     </header>
-  )
-}//end Header
+  );
+}; //end Header
 
 export default Header;
