972fcf91db9848c7901baac7f9c07293e2ab98e3
scott fuston <fuston05@yahoo.com>
2021-05-28 09:53:12 -0400

    edits the about section text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 
 const About: React.FC = () => {
-  return (
-    <section className="aboutCont">
-      {/* eslint-disable-next-line */}
-      <a id="about"></a>
-      <div className="aboutText">
-        <h4 className="sectionTitle">About Me</h4>
-        <p>
-          I am a Full Stack Web Developer and recent Lambda School Graduate. I
-          don't even have the words to express how excited I am to begin
-          searching for my dream job! I am so passionate about every aspect of
-          software development. It's very gratifying to see a complex problem
-          solved in an elegant way. I love seeing a project come together after
-          all the hard work that goes into it! And probably most of all, I am always looking forward to learning new things!
-        </p>
-        <br />
-        <p>
-          For me It's not just about the code, cross-functional team
-          collaboration is an incredible experience that has a way of bringing
-          people together and building great friendships. A good, diverse team
-          leads to increased creativity, productivity, and often to some very
-          unique implementations.
-        </p>
-        <br />
-        <p>
-          Check out my skills below, and get in touch with me. I look forward to
-          discussing an amazing opportunity with you.
-        </p>
-      </div>
-    </section>
-  );
+    return (
+        <section className="aboutCont">
+            {/* eslint-disable-next-line */}
+            <a id="about"></a>
+            <div className="aboutText">
+                <h4 className="sectionTitle">About Me</h4>
+                <p>
+                    I am a Full Stack Web Developer and recent Lambda School
+                    Graduate. I don't even have the words to express how excited
+                    I am to begin searching for my dream job! I am so passionate
+                    about every aspect of software development. It's very
+                    gratifying to see a complex problem solved in an elegant
+                    way. I love seeing a project come together after all the
+                    hard work that goes into it! And probably most of all, I am
+                    always looking forward to learning new things!
+                </p>
+                <br />
+                <p>
+                    For me It's not just about the code, cross-functional team
+                    collaboration is an incredible experience that has a way of
+                    bringing people together and building great friendships. A
+                    good, diverse team leads to increased creativity,
+                    productivity, and often to some very unique implementations.
+                </p>
+                <br />
+                <p>
+                    Check out my skills and links below. I look forward to
+                    discussing an amazing opportunity with you. You can get in
+                    touch with me and download my resume below.
+                </p>
+            </div>
+        </section>
+    );
 }; //end About
 
 export default About;
