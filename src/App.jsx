452a43df57778f3c929943856120d27d4bb6ab10
scott fuston <fuston05@yahoo.com>
2021-09-20 18:33:04 -0400

    changes a few more file ext that i missed from TS to JS and JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
+import React from "react";
 
 //components
-import {Header} from './components/Common';
-import {Footer} from './components/Common';
-import {ToTopArrow} from './components/Common';
+import {Header} from "./components/Common";
+import {Footer} from "./components/Common";
+import {ToTopArrow} from "./components/Common";
 
-import Portfolio from './components/Portfolio';
-import About from './components/About';
-import Contact from './components/Contact';
-import Skills from './components/Skills';
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
 
-const App: React.FC= () => {
-
-  return (
-    <div className="App">
-      {/* anchor used by the 'to-top' arrow */}
-      {/* eslint-disable-next-line */}
-      <a id='top' />
-      <Header />
-      <Portfolio />
-      <About />
-      <Skills />
-      <Contact/>
-      <Footer />
-      <ToTopArrow />
-    </div>
-  );
-}//end App
+const App = () => {
+    return (
+        <div className="App">
+            {/* anchor used by the 'to-top' arrow */}
+            {/* eslint-disable-next-line */}
+            <a id="top" />
+            <Header />
+            <Portfolio />
+            <About />
+            <Skills />
+            <Contact />
+            <Footer />
+            <ToTopArrow />
+        </div>
+    );
+};
 
 export default App;
