79697dc632eec8bda57c0c9980cb9c4d510aad61
scott fuston <fuston05@yahoo.com>
2021-10-06 10:10:11 -0400

    adds a subtle rotation animation to the 2 skills icons in the skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -28,10 +28,10 @@ const Skills = () => {
             <div className="skills">
                 <span className="skillsHeader">
                     {/* FontAwesome Icon */}
-                    <i className="fab fa-git-alt skillsIcon"></i>
+                    <i className="fab fa-git-alt skillsIcon rotate"></i>
 
                     <h5 title="Comfortable Skills">Check it out!</h5>
-                    <i className="fab fa-react skillsIcon"></i>
+                    <i className="fab fa-react skillsIcon rotate"></i>
                 </span>
 
                 <div className="skillSection">
