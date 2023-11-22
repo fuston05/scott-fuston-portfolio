fda9205e1d287047b9cd57e11c77e2c757205e7e
Scott Fuston <scott.fuston@grandpad.net>
2023-11-22 09:14:40 -0500

    update footer copyright

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footerCont">
             <div className="copyCont">
-                <span>&copy;</span> <p>Copyright Scott Fuston 2020 - 2021</p>
+                <span>&copy;</span> <p>Copyright Scott Fuston 2020 - 2024</p>
             </div>
         </footer>
     );
