f9246365db6c78b3494f09c7e91b978cc3d951ab
Scott Fuston <scott.fuston@grandpad.net>
2024-12-11 15:42:11 -0500

    update footer, and skills

--- src/assets/data/skillsData.js
+++ src/assets/data/skillsData.js
@@ -1,11 +1,12 @@
 export const topSkills = [
     { name: "Cypress Automation", proficiency: 9 },
     { name: "Mobile Automation", proficiency: 9 },
-    { name: "JavaScript(ES6)", proficiency: 8 },
-    { name: "NodeJs", proficiency: 7 },
+    { name: "JavaScript(ES6)", proficiency: 9 },
+    { name: "TypeScript", proficiency: 9 },
+    { name: "NodeJs", proficiency: 9 },
     { name: "Python", proficiency: 3 },
     { name: "Java", proficiency: 4 },
     { name: "React", proficiency: 8 },
     { name: "Vue", proficiency: 3 },
-    { name: "Responsive Design", proficiency: 10 },
+    { name: "Responsive Design", proficiency: 8 },
 ];
--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footerCont">
             <div className="copyCont">
-                <span>&copy;</span> <p>Copyright Scott Fuston 2020 - 2024</p>
+                <span>&copy;</span> <p>Copyright Scott Fuston 2020 - 2025</p>
             </div>
         </footer>
     );
